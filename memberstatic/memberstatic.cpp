64cb8b6ec12e69880454fe08240b95e95f805ab0
andiharun29 <andiharunalrasyid14@gmail.com>
2023-05-16 23:03:14 +0700

    membuat member dalam class

--- memberstatic/memberstatic.cpp
+++ memberstatic/memberstatic.cpp
@@ -3,4 +3,12 @@
 using namespace std;
 
 class mahasiswa {
+public:
+	static int nim;
+	int id;
+	string nama;
 
+	void setID();
+	void printAll();
+	mahasiswa(string pnama) :nama(pnama) { setID();  }
+};
