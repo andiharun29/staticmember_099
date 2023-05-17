e9a4d50c1c46a32307701835a14610319d3434a9
andiharun29 <andiharunalrasyid14@gmail.com>
2023-05-17 17:36:52 +0700

    memanggil semua destructor

--- destructor/destructor.cpp
+++ destructor/destructor.cpp
@@ -38,3 +38,10 @@ void angka::isiData() {
 	cout << endl;
 }
 
+int main() {
+	angka belajarcpp(3); //constructor dipanggil
+	angka* ptrBelajarcpp = new angka(5); // Constructor dipanggil
+	delete ptrBelajarcpp; //Destructor dipanggil
+
+	return 0;
+} //Destructor dipanggil
